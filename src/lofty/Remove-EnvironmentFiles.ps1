a7f1b234f00bf6fff509295d0ed9c96a6e3d64fd
Yannis Guedel <yannis.guedel@unic.com>
2014-11-07 13:55:11 +0100

    added Remove-EnvironmentFiles

---
+++ src/lofty/Remove-EnvironmentFiles.ps1
@@ -0,0 +1,20 @@
+function Remove-EnvironmentFiles
+{
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$true)]
+    [string] $WebRoot,
+    [Parameter(Mandatory=$true)]
+    [string] $Environment,
+    [Parameter(Mandatory=$true)]
+    [string] $Role
+
+  )
+  Process
+  {
+    $config  = Get-ScProjectConfig $WebRoot
+    $pattern = Get-RubblePattern -Pattern $config.KeepAppConfigIncludes -Replacement @{'$Envrionemnt'= $Environment; '$Role' = $Role }
+
+    Remove-RubbleItem -Folder $WebRoot -Pattern $pattern
+  }
+}
