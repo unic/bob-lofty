4cfde983afed72126b143d4914e3734f8d0f36f4
Yannis Guedel <yannis.guedel@unic.com>
2015-01-23 15:46:41 +0100

    added clear folders

---
+++ src/lofty/Clear-Folders.ps1
@@ -0,0 +1,46 @@
+<#
+.SYNOPSIS
+Clears the content of all folders provided.
+
+.DESCRIPTION
+Clears the content of all folders provided.
+
+.PARAMETER Folders
+A semicolon separated list of folders to delete. If the $Website is specified
+relative paths must be relative to this, else from the current working
+directory.
+
+.PARAMETER Website
+If specified, this is the starting point of all relative folders.
+
+.EXAMPLE
+Clear-Folders -Folders "..\relativePath;D:\data\absolute\path"
+
+#>
+function Clear-Folders
+{
+    [CmdletBinding()]
+    Param(
+        [string] $Folders,
+        [string] $Website
+    )
+    Process
+    {
+        $normalFolders = $Folders.Split(";")
+        if($Website) {
+            Push-Location $Website
+        }
+        foreach($folder in $normalFolders) {
+            if($folder) {
+                if(Test-Path $folder) {
+                    $folder = Resolve-Path $folder
+                    Write-Verbose "Delete $folder"
+                    rm "$folder\*" -Recurse
+                }
+            }
+        }
+        if($Website) {
+            Pop-Location
+        }
+    }
+}
