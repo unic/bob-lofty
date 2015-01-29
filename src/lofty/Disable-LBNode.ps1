2c12455e2d3152b374130182aef5bb4fd69da65c
Yannis Guedel <yannis.guedel@unic.com>
2015-01-29 09:14:03 +0100

    implemented feedback from PR

--- src/lofty/Disable-LBNode.ps1
+++ src/lofty/Disable-LBNode.ps1
@@ -1,11 +1,11 @@
 <#
 .SYNOPSIS
-Disables a node in the load balancer and waits until connections drops to specific per-cent.
+Disables a node in the load balancer and waits until connections drop to specific percent.
 
 .DESCRIPTION
 Disables a node in the load balancer and waits until connections on the node
-drops to specific percent of the original connections. So if before disabling
-the node there are 100 connections and $WaitPerCent is 0.15, Disable-LBNode
+drop to specific percent of the original connections. So if before disabling
+the node there are 100 connections and $WaitPercent is 0.15, Disable-LBNode
 will wait until there are less than 15 connections left.
 
 .PARAMETER PoolName
@@ -14,16 +14,16 @@ The name of the pool containing the node to disable.
 .PARAMETER MemberName
 The name of the member to disable on the load balancer.
 
-.PARAMETER WaitPerCent
-After disabling the node, Disable-LBNode will wait until the connections drops
-to $WaitPerCent of the original connections.
+.PARAMETER WaitPercent
+After disabling the node, Disable-LBNode will wait until the connections drop
+to $WaitPercent of the original connections.
 The percent must be specified in decimal form. So 15% is 0.15.
 
 .PARAMETER TimeoutSeconds
 The maximum number of seconds to wait.
 
 .EXAMPLE
-Disable-LBNode -PoolName mypool -MemberName mynode -WaitPerCent 0.15 -TimeoutSeconds 600 
+Disable-LBNode -PoolName mypool -MemberName mynode -WaitPercent 0.15 -TimeoutSeconds 600
 
 #>
 function Disable-LBNode
@@ -35,7 +35,7 @@ function Disable-LBNode
         [Parameter(Mandatory=$true)]
         [string] $MemberName,
         [Parameter(Mandatory=$true)]
-        [float] $WaitPerCent,
+        [float] $WaitPercent,
         [Parameter(Mandatory=$true)]
         [int] $TimeoutSeconds
     )
@@ -48,7 +48,7 @@ function Disable-LBNode
         $ctrl.LocalLBPool.set_member_session_enabled_state($node.Pool, $node.AddressPort, "STATE_DISABLED")
 
         if($startConnections -ne 0) {
-            [int]$targetConnections = $startConnections * $WaitPerCent
+            [int]$targetConnections = $startConnections * $WaitPercent
             Write-Verbose "Node had $startConnections connections. Waiting until they drop below $targetConnections"
             $connections = Get-LBNodeConnections $node.Pool $node.IPPortDefinition
             $startTime = Get-Date
