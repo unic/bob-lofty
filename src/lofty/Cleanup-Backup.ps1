7ad209d638caecdc376f3feba9ada59d47aea73d
Yannis Guedel <yannis.guedel@unic.com>
2015-03-18 08:49:39 +0100

    added minimum backup number

--- src/lofty/Cleanup-Backup.ps1
+++ src/lofty/Cleanup-Backup.ps1
@@ -14,11 +14,15 @@ function Cleanup-Backup
     [CmdletBinding()]
     Param(
         [string] $BackupLocation = "D:\Backup\OctopusDeploy",
-        [int] $MaximumBackupDays = 7
+        [int] $MaximumBackupDays = 7,
+        [int] $MinimumBackups = 5
     )
     Process
     {
-        Get-Backup | ? {(Get-Date) - $_.LastWriteTime -gt [TimeSpan]::FromDays($MaximumBackupDays)} | % {rm $_.FullName}
-         #? {(Get-Date) - $_.LastWriteTime -gt $MaximumBackupDays} | % {rm $_.FullName}
+        Get-Backup |
+            sort {$_.LastWriteTime} -Descending |
+            select -skip $MinimumBackups |
+            ? {(Get-Date) - $_.LastWriteTime -gt [TimeSpan]::FromDays($MaximumBackupDays)} |
+            % {Write-Verbose "Delete $($_.FullName)"; rm $_.FullName}
     }
 }
