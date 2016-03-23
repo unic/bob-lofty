b4d4a79c6d09213b29234c37d2520bb439192b6d
Yannis Guedel <yannis.guedel@unic.com>
2016-03-23 17:29:40 +0100

    implemented default items creation

---
+++ src/lofty/Install-DefaultItems.ps1
@@ -0,0 +1,24 @@
+function Install-DefaultItems {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string] $Url,
+        [Parameter(Mandatory=$true)]
+        [string] $ItemReferencesPath,
+        [Parameter(Mandatory=$true)]
+        [string] $TempItemspath
+    )
+    Process
+    {
+        $tempPath = "$TempItemspath\$([Guid]::NewGuid())"
+        mkdir $tempPath
+        Expand-RubbleArchive ".\appDefault.zip" $tempPath 
+        
+        $updatePackageFile =  "$tempPath\package.update"
+        New-ScSerializationPackage -Source "$tempPath\appDefault" -Target $ItemReferencesPath -OutputFile $updatePackageFile -CollisionMode "Skip"
+        
+        Install-ScSerializationPackage -Path $updatePackageFile -Url $Url
+        
+        rm $tempPath -Recurse
+    }
+}
---
+++ tools/sitecore-courier/README.txt
@@ -0,0 +1,9 @@
+Since Sitecore Courier has no proper release management and we need some extra features,
+we use a custom build here.
+
+Custom features:
+- New command line option '-collisionBehavior' with possible values force, skip and undefined
+- All file system folders are excluced
+
+The current binaries are a release build of the commit 82aea7b60d
+(https://github.com/unic/Sitecore-Courier/tree/unicMaster)
